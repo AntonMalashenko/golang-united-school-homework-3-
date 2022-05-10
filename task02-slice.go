82734f9c520c948e079751112f67bd9958ba75a1
anton_malashenko <anton.mal.slb@gmail.com>
2022-05-10 19:57:29 +0300

    add slice solution

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,11 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	inputLen := len(input)
+	result = make([]int64, inputLen)
+	for idx, value := range input {
+		index := inputLen - idx - 1
+		result[index] = value
+	}
+	return result
 }
